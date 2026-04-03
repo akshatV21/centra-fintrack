149ecb5384431769ffb2138b54762a95cbe8b64c
Akshat Vishwakarma <a.vishwakarma2110@gmail.com>
2026-04-03 15:30:43 +0530

    chore(cors)

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,6 @@ async function bootstrap() {
   const PORT = config.getOrThrow('PORT')
   const logger = new Logger('Bootstrap')
 
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true })
-
   app.use(helmet())
   app.use(morgan('dev'))
   app.use(cookieParser())
